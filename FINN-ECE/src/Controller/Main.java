9c6d08ec7956d836a19a68e08c186fcc4748d6d4
LOUL29 <lavergne.louis@gmail.com>
2021-12-04 12:01:30 +0100

    deplacement level 2
    transfo glace epaisse en glace fine apres passage de eceman

--- FINN-ECE/src/Controller/Main.java
+++ FINN-ECE/src/Controller/Main.java
@@ -6,6 +6,7 @@
 package Controller;
 
 import View.Level1;
+import View.Level2;
 import java.util.Scanner;
 
 /**
@@ -25,7 +26,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         
         //Level one
-        Level1 first_board = new Level1(15,19);
+        /*Level1 first_board = new Level1(15,19);
         first_board.DisplayLevel1();
         
         while (i < 1000){            
@@ -33,9 +34,18 @@ public class Main {
             char key = sc.next().charAt(0);           
             key_move.Move1(key);    
             i++;
-        }
+        }*/
         
-        //Level two
+        //Level two        
+        Level2 second_board = new Level2(15,19);
+        second_board.DisplayLevel2();
+        
+        while (i < 1000){            
+            System.out.println("choose a move : z (up) / q (left) / s (down) / d (right) -- then press enter to valide");            
+            char key = sc.next().charAt(0);           
+            key_move.Move2(key);    
+            i++;
+        }
         
         //Level three
         
--- FINN-ECE/src/View/Level2.java
+++ FINN-ECE/src/View/Level2.java
@@ -14,10 +14,57 @@ public class Level2 {
     private int nb_col;
     private int nb_lin;
     private char board[][];
+    private char tmp;
 
     public Level2(int nb_col, int nb_lin) {
         this.nb_col = nb_col;
         this.nb_lin = nb_lin;
+        
+        board = new char[nb_col][nb_lin];
+        
+        //Empty display
+        for(int i = 0; i < nb_col; i++){
+            for(int j = 0; j < nb_lin; j++){
+                
+                board[i][j] = '#';
+            }
+        }
+        
+        //Wall display
+        for(int k = 7; k <= 9; k++){
+            
+            board[k][1] = 'M';
+        }
+        for(int k = 7; k <= 9; k++){
+            
+            board[k][15] = 'M';
+        }
+        for(int k = 1; k <= 15; k++){
+            
+            board[7][k] = 'M';
+        }
+        for(int k = 1; k <= 15; k++){
+            
+            board[9][k] = 'M';
+        }
+        
+        //Ice display
+        for(int k = 8; k <= 13; k++){
+            
+            board[8][k] = 'o';
+        }
+        
+        //Strong ice display
+        for(int k = 3; k <= 7; k++){
+            
+            board[8][k] = 'O';
+        }
+        
+        //Portal display
+        board[8][2] = 'P';
+        
+        //Eceman display
+        board[8][14] = 'E';
     }
     
     public void DisplayLevel2(){ //Board display in console
@@ -39,9 +86,17 @@ public class Level2 {
         for(int i = 0; i < 15; i++){           
             for(int j = 0; j < 19; j++){
                 if(board[i][j] == 'E'){ //Found eceman on the board
-                    if(board[i][j-1] == 'o' || board[i][j-1] == 'P'){ //Test the legitimcy of the movement
-                        board[i][j] = 'w'; //Remplace ice by water after eceman walk on it
-                        board[i][j-1] = 'E'; //Remplace the legit case by eceman
+                    if(board[i][j-1] == 'o' || board[i][j-1] == 'O'){ //Test the legitimcy of the movement
+                        if(tmp == 'O'){
+                            this.tmp = board[i][j-1];
+                            board[i][j] = 'o'; //Remplace ice by water after eceman walk on it
+                            board[i][j-1] = 'E'; //Remplace the legit case by eceman
+                        }
+                        else{
+                            this.tmp = board[i][j-1];
+                            board[i][j] = 'w'; //Remplace ice by water after eceman walk on it
+                            board[i][j-1] = 'E'; //Remplace the legit case by eceman
+                        }                        
                     }
                     else{
                         System.out.println("no legit movement, please try again");
@@ -56,9 +111,17 @@ public class Level2 {
         for(int i = 0; i < 15; i++){           
             for(int j = 0; j < 19; j++){
                 if(board[i][j] == 'E'){ //Found eceman on the board
-                    if(board[i][j+1] == 'o' || board[i][j+1] == 'P'){ //Test the legitimcy of the movement
-                        board[i][j] = 'w'; //Remplace ice by water after eceman walk on it
-                        board[i][j+1] = 'E'; //Remplace the legit case by eceman
+                    if(board[i][j+1] == 'o' || board[i][j+1] == 'O'){ //Test the legitimcy of the movement
+                        if(tmp == 'O'){
+                            this.tmp = board[i][j+1];
+                            board[i][j] = 'o'; //Remplace ice by water after eceman walk on it
+                            board[i][j+1] = 'E'; //Remplace the legit case by eceman
+                        }
+                        else{
+                            this.tmp = board[i][j+1];
+                            board[i][j] = 'w'; //Remplace ice by water after eceman walk on it
+                            board[i][j+1] = 'E'; //Remplace the legit case by eceman
+                        }                        
                     }
                     else{
                         System.out.println("no legit movement, please try again");
@@ -73,9 +136,17 @@ public class Level2 {
         for(int i = 0; i < 15; i++){           
             for(int j = 0; j < 19; j++){
                 if(board[i][j] == 'E'){ //Found eceman on the board
-                    if(board[i-1][j] == 'o' || board[i-1][j] == 'P'){ //Test the legitimcy of the movement
-                        board[i][j] = 'w'; //Remplace ice by water after eceman walk on it
-                        board[i-1][j] = 'E'; //Remplace the legit case by eceman
+                    if(board[i-1][j] == 'o' || board[i-1][j] == 'O'){ //Test the legitimcy of the movement
+                        if(tmp == 'O'){
+                            this.tmp = board[i-1][j];
+                            board[i][j] = 'o'; //Remplace ice by water after eceman walk on it
+                            board[i-1][j] = 'E'; //Remplace the legit case by eceman
+                        }
+                        else{
+                            this.tmp = board[i-1][j];
+                            board[i][j] = 'w'; //Remplace ice by water after eceman walk on it
+                            board[i-1][j] = 'E'; //Remplace the legit case by eceman
+                        }                        
                     }
                     else{
                         System.out.println("no legit movement, please try again");
@@ -90,9 +161,17 @@ public class Level2 {
         for(int i = 0; i < 15; i++){           
             for(int j = 0; j < 19; j++){
                 if(board[i][j] == 'E'){ //Found eceman on the board
-                    if(board[i+1][j] == 'o' || board[i+1][j] == 'P'){ //Test the legitimcy of the movement
-                        board[i][j] = 'w'; //Remplace ice by water after eceman walk on it
-                        board[i+1][j] = 'E'; //Remplace the legit case by eceman
+                    if(board[i+1][j] == 'o' || board[i+1][j] == 'O'){ //Test the legitimcy of the movement
+                        if(tmp == 'O'){
+                            this.tmp = board[i+1][j];
+                            board[i][j] = 'o'; //Remplace ice by water after eceman walk on it
+                            board[i+1][j] = 'E'; //Remplace the legit case by eceman
+                        }
+                        else{
+                            this.tmp = board[i+1][j];
+                            board[i][j] = 'w'; //Remplace ice by water after eceman walk on it
+                            board[i+1][j] = 'E'; //Remplace the legit case by eceman
+                        }
                     }
                     else{
                         System.out.println("no legit movement, please try again");
